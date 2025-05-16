f30887b5f81e0b456006dc6c02dd8880c466d851
Pedro Henrique <pedroaraujoaraujo9191@gmail.com>
2025-05-16 16:43:22 -0300

    Adicionando a classe Playlist ao modelo

---
+++ src/main/java/model/Playlist.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Playlist {
+    private int id;
+    private String nome;
+    private int usuarioId;
+
+    public Playlist(int id, String nome, int usuarioId) {
+        this.id = id;
+        this.nome = nome;
+        this.usuarioId = usuarioId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+}
