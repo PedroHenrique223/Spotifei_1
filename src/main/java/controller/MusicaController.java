cc38da6133c52e7b68c0f85856a7d37971428ea1
Pedro Henrique <pedroaraujoaraujo9191@gmail.com>
2025-05-16 17:25:13 -0300

    Adicionando classe MusicaController para manipulação de músicas

---
+++ src/main/java/controller/MusicaController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import dao.MusicaDAO;
+import model.Musica;
+import java.sql.Connection;
+import java.util.List;
+
+public class MusicaController {
+    private MusicaDAO musicaDAO;
+
+    public MusicaController(Connection connection) {
+        this.musicaDAO = new MusicaDAO(connection);
+    }
+
+    public boolean cadastrarMusica(String nome, String artista, String genero) {
+        Musica musica = new Musica(0, nome, artista, genero);
+        return musicaDAO.create(musica);
+    }
+
+    public List<Musica> listarMusicas() {
+        return musicaDAO.read();
+    }
+    
+    public boolean removerMusica(int id) {
+    return musicaDAO.delete(id);
+}
+
+    
+}
