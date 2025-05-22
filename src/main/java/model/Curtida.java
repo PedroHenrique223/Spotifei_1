3badee3ad378c3c8263d114303c0c68f2470fb61
Pedro Henrique <pedroaraujoaraujo9191@gmail.com>
2025-05-21 22:17:05 -0300

    att do codigo model Curtida

--- src/main/java/model/Curtida.java
+++ src/main/java/model/Curtida.java
@@ -7,6 +7,22 @@ public class Curtida {
     private int idUsuario;
     private int idMusica;
     private Timestamp dataCurtida;
+    private String tituloMusica; // para exibir no histórico
 
-    
+    // getters e setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getIdUsuario() { return idUsuario; }
+    public void setIdUsuario(int idUsuario) { this.idUsuario = idUsuario; }
+
+    public int getIdMusica() { return idMusica; }
+    public void setIdMusica(int idMusica) { this.idMusica = idMusica; }
+
+    public Timestamp getDataCurtida() { return dataCurtida; }
+    public void setDataCurtida(Timestamp dataCurtida) { this.dataCurtida = dataCurtida; }
+
+    public String getTituloMusica() { return tituloMusica; }
+    public void setTituloMusica(String tituloMusica) { this.tituloMusica = tituloMusica; }
 }
+
