785656987ee1b79236e41e4b14f551cf7fbd7557
Pedro Henrique <pedroaraujoaraujo9191@gmail.com>
2025-05-21 22:13:29 -0300

    correção do upload dos codigos no github

--- src/main/java/dao/CurtidaDAO.java
+++ src/main/java/dao/CurtidaDAO.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author Asus
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class CurtidaDAO {
-    
+    private Connection conexao;
+
+    public CurtidaDAO(Connection conexao) {
+        this.conexao = conexao;
+    }
+
+    public void curtirMusica(int idUsuario, int idMusica) throws Exception {
+        String sql = "INSERT INTO curtida (id_usuario, id_musica) VALUES (?, ?)";
+        PreparedStatement ps = conexao.prepareStatement(sql);
+        ps.setInt(1, idUsuario);
+        ps.setInt(2, idMusica);
+        ps.executeUpdate();
+    }
+
+    public void descurtirMusica(int idUsuario, int idMusica) throws Exception {
+        String sql = "DELETE FROM curtida WHERE id_usuario = ? AND id_musica = ?";
+        PreparedStatement ps = conexao.prepareStatement(sql);
+        ps.setInt(1, idUsuario);
+        ps.setInt(2, idMusica);
+        ps.executeUpdate();
+    }
+
+    public boolean verificarCurtida(int idUsuario, int idMusica) throws Exception {
+        String sql = "SELECT * FROM curtida WHERE id_usuario = ? AND id_musica = ?";
+        PreparedStatement ps = conexao.prepareStatement(sql);
+        ps.setInt(1, idUsuario);
+        ps.setInt(2, idMusica);
+        ResultSet rs = ps.executeQuery();
+        return rs.next();
+    }
 }
--- src/main/java/model/Curtida.java
+++ src/main/java/model/Curtida.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Asus
- */
+import java.sql.Timestamp;
+
 public class Curtida {
+    private int id;
+    private int idUsuario;
+    private int idMusica;
+    private Timestamp dataCurtida;
+
     
 }
